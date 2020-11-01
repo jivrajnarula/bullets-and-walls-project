6d3e70b78d3017c7a4ca406d0e2e0e1289160a68
jivrajnarula <70409178+jivrajnarula@users.noreply.github.com>
2020-11-01 14:30:24 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,42 +7,21 @@ function setup() {
   //createSprite(400, 200, 50, 50);
   thickness=random(22,83)
   speed=random(223,321)
-  weight=random(30,52)
-  thickness=createSprite(50,200,50,50);
-  bullet=createSprite(50,200,50,50);
-  bullet.shapeColor="white"
-  wall=createSprite(1200,200,thickness,height/2);
-  wall.shapeColor="blue"
+  
+  bullet=createSprite(50,200,50,5);
+  bullet.shapeColor=color(255)
   bullet.velocityX = speed;
+
+
+  wall=createSprite(1200,200,thickness,height/2);
+  wall.shapeColor=color(230,230,230)
+  
  
 
 }
 
 function draw() {
 background(0);  
-if(wall.x-bullet.x < bullet.width/2+ wall.width/2) 
-{
-bullet.velocityX=0
-  var deformation= 0.5 * weight * speed* speed/22509;
-  if(deformation>180) 
-{
-  bullet.shapeColor=color(255,0,0);
-}
-
-if(deformation<180 && deformation>100)
-{
-
-  bullet.shapeColor=color(230,230,0);
-}
-
-if(deformation<100)
-{
-
-  bullet.shapeColor=color(0,255,0);
- }
-
-
-}
 if(hasCollided(bullet,wall))
 {
   bullet.velocityX=0;
@@ -71,9 +50,7 @@ function hasCollided(lbullet,lwall)
    if (bulletRightEdge>=wallLeftEdge)
    {
       return true
-   }
-   else 
-   {
+      
       return false;
   }
 }
